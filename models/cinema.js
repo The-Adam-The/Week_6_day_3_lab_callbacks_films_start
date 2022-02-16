fa215eece273f86f415a171f55d8328bd2764e66
codeclan student <codeclanstudent@codeclans-mbp.home>
2022-02-16 17:30:54 +0000

    added semicolons

--- models/cinema.js
+++ models/cinema.js
@@ -8,33 +8,33 @@ Cinema.prototype.filmNames = function() {
       output.push(film.title);
     }) 
     return output;
-}
+};
 
 Cinema.prototype.findByTitle = function(filmName) {
   const output = this.films.filter(film => film.title === filmName);
   return output;
-}
+};
 
 Cinema.prototype.findByGenre = function(genre) {
   const output = this.films.filter(film => film.genre === genre);
   return output;
-}
+};
 
 Cinema.prototype.areThereAnyFilmsFromParticularYear = function(year) {
   const output = this.films.some(film => film.year === year);
   return output;
-}
+};
 
 Cinema.prototype.areFilmsOverParticularLength = function(length) {
   const output = this.films.every(film => film.length > length);
   return output;
-}
+};
 
 Cinema.prototype.sumOfAllFilmsRunTime = function() {
   const sum = this.films.reduce((runningTotal, film) => {
-    return runningTotal += film.length;
+    return runningTotal + film.length;
   }, 0);
   return sum;
-}
+};
 
 module.exports = Cinema;
