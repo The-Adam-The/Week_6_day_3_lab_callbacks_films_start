c76849d61447e1fa6a3e01d648298e5e1914b6e7
codeclan student <codeclanstudent@codeclans-mbp.home>
2022-02-16 14:06:55 +0000

    first test completed

--- models/cinema.js
+++ models/cinema.js
@@ -2,4 +2,12 @@ const Cinema = function (films) {
   this.films = films;
 };
 
+Cinema.prototype.filmNames = function() {
+    output = [];
+    this.films.forEach((film) => {
+      output.push(film.title);
+    }) 
+    return output
+}
+
 module.exports = Cinema;
